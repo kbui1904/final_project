a7921336bbe313aec885b92ef7c3cc8ae266cba7
kbui1904 <99047685+kbui1904@users.noreply.github.com>
2022-05-06 20:46:53 +0200

    Update final_v4.py

--- final_v4.py
+++ final_v4.py
@@ -19,7 +19,7 @@ FROM countries''', con)
 #FR-2.14 + FR-2.15: input country acronym and validate selection
 
 country_selected = st.selectbox('Please choose the country acronym:', df_countries.loc[:,'Acronym'].tolist())
-country_name = df_countries.loc[df_countries[Acronym]==country_selected, "Country"]
+country_name = df_countries.loc[df_countries['Acronym']==country_selected, ["Country"]]
 st.write(country_name)
 
 #FR-2.16: Connect to the database and create grants_per_participants dataframe
