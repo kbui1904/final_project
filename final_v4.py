45094cf99507beb59582321d8af50f45703936f8
kbui1904 <99047685+kbui1904@users.noreply.github.com>
2022-05-06 20:23:13 +0200

    Add files via upload

---
+++ final_v4.py
@@ -0,0 +1,52 @@
+#Kiet Bui and Jack Katene
+import pandas as pd
+import sqlite3
+import streamlit as st
+
+st.header("Final project")
+
+#FR-2.13: Connect to the database and extract list of countries in a dataframe
+
+con = sqlite3.connect('ecsel_database.db')
+df_countries = pd.read_sql('''SELECT Country, Acronym
+FROM countries''', con)
+
+'''df_countries'''
+
+#FR-2.14 + FR-2.15: input country acronym and validate selection
+
+'''country_selected = str(input("Please select your desired country: "))
+while country_selected not in df_countries.loc[:,'Acronym'].tolist():
+    print("Invalid country")
+    country_selected = str(input("Please select your desired country: "))
+'''
+    
+country_selection = st.selectbox('Please choose the country acronym:', df_countries.loc[:,'Acronym'].tolist())
+st.write(country_selection)
+
+#FR-2.16: Connect to the database and create grants_per_participants dataframe
+#FR-2.17: Display the dataframe
+
+con = sqlite3.connect('ecsel_database.db')
+df_participants = pd.read_sql('''SELECT shortName, name, activityType, organizationURL, ecContribution, country 
+FROM participants
+INNER JOIN projects
+ON participants.projectID = projects.projectID''', con)
+
+df_filtered = df_participants[df_participants["country"] == country_selected]
+
+df_grants_per_partner = df_filtered.groupby(['country', 'shortName', 'name', 'activityType', 'organizationURL'], as_index=False).agg({'ecContribution': ['count', 'sum']})
+df_grants_per_partner = df_grants_per_partner.sort_values([("ecContribution", "sum")], ascending=False)
+'''df_grants_per_partner'''
+
+#FR-2.18: Generate Dataframe with project coordinators from the selected country
+
+con = sqlite3.connect('ecsel_database.db')
+df_project_coordinators = pd.read_sql('''SELECT country, shortName, name, activityType, projectAcronym 
+FROM participants
+WHERE role = 'coordinator'
+''', con)
+
+df_project_coordinators = df_project_coordinators[df_project_coordinators["country"] == country_selected]
+df_project_coordinators = df_project_coordinators.sort_values(by=['shortName'], ascending=True)
+'''df_project_coordinators'''
